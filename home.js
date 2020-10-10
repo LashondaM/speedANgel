28e115cdae8608b81e72176b050f902669ac4b83
Lashonda <lashondamiller007@gmail.com>
2020-10-09 21:58:44 -0400

    update1

---
+++ home.js
@@ -0,0 +1,22 @@
+// image overlay 
+
+
+// var posts = [{
+//     picture: "https://cutewallpaper.org/21/lamborghini-wallpaper/Black-Lamborghini-Wallpaper-72-images-.jpg",
+//     title: "Need for Speed?"
+// }]
+
+
+// var postHTML = '' 
+// for(var i = 0; i < posts.length; i++){
+//     var image = "<div class='card bg-dark text-white'><img src= "+posts[i].title+ " class='card-img' ></img>";
+//     var heading = "<div class='card-img-overlay'><h2>" +posts[i].title+ "</h2></div></div>";
+//     var concatThis = image + heading;
+//     postHTML += concatThis;
+// };
+
+
+// console.log(postHTML);
+
+// document.getElementById("love").innerHTML = postHTML;
+// image overlay end
