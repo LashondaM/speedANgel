3cfe98d0eed619fc4d2d76f6a63f02923da8f223
Lashonda <lashondamiller007@gmail.com>
2020-10-28 22:16:17 -0400

    homepage update

--- home.js
+++ home.js
@@ -1,22 +1,31 @@
-// image overlay 
+var featuring = [
+
+    {
+        title: "Infiniti",
+        id: "feat1",
+        picture: "https://s3.amazonaws.com/www.autowise.com/wp-content/uploads/2018/05/30160952/project-black-s-e1527713885127.jpg"
+    },
+    {
+        title: "Lamborghini",
+        id: "feat2",
+        picture: "https://www.fckerbecklamborghini.com/imagetag/11461/2/l/New-2020-Lamborghini-Huracan-EVO-LP-640-4-EVO.jpg"
+    },
+    {
+        title: "Lotus",
+        id: "feat3",
+        picture: "https://www.autocar.co.uk/sites/autocar.co.uk/files/images/car-reviews/first-drives/legacy/3-lotus-v6-hybrid-esprit-render-2020-static-front.jpg"
+    }
+
+]
+
+var postHTML = " "
 
 
-// var posts = [{
-//     picture: "https://cutewallpaper.org/21/lamborghini-wallpaper/Black-Lamborghini-Wallpaper-72-images-.jpg",
-//     title: "Need for Speed?"
-// }]
+for(var i = 0; i < featuring.length; i++){
+    var heading = '<div class="cars ' + featuring[i].id + '"><span><h5 class="text-white">' + featuring[i].title + '</h5>'
+    var image = '<img class="feats" src=" ' + featuring[i].picture + ' "/ </span></div>'
+    var concatThis = heading + image
+    postHTML += concatThis
+}
 
-
-// var postHTML = '' 
-// for(var i = 0; i < posts.length; i++){
-//     var image = "<div class='card bg-dark text-white'><img src= "+posts[i].title+ " class='card-img' ></img>";
-//     var heading = "<div class='card-img-overlay'><h2>" +posts[i].title+ "</h2></div></div>";
-//     var concatThis = image + heading;
-//     postHTML += concatThis;
-// };
-
-
-// console.log(postHTML);
-
-// document.getElementById("love").innerHTML = postHTML;
-// image overlay end
+document.getElementById('features').innerHTML = postHTML
