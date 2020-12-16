2b502e78f1fe975db9a38cf743a61223e770552c
Lashonda <lashondamiller007@gmail.com>
2020-12-16 14:17:07 -0500

    more updates

--- infiniti.js
+++ infiniti.js
@@ -5,6 +5,9 @@ $(window).scroll(function(){
     console.log(wScroll)
     $('.tres').css('top', -8 + (wScroll*.005)+'em')
     $('.dice').css('top', -3 + (wScroll*.005)+'em')
+
+    $('.apple').css('bottom', -3 + (wScroll*.005)+'em')
+    $('.pear').css('top', -1 + (wScroll*.005)+'em')
     // $('.dice').css('left', -3 + (wScroll*.005)+'em')
     
 })
